5b108f1a64847ca3798f8ee7afe8729e9a228406
ritesh2204 <rvritesh11@gmail.com>
2020-06-28 15:29:21 +0530

    added media queries

--- src/Theme/theme.js
+++ src/Theme/theme.js
@@ -1,10 +1,13 @@
 export default {
   colors: {
     black: "#000",
-    green: "#888"
+    green: "#888",
   },
 
   mediaQueries: {
-    below786: `only screen and (max-width: 786px)`
-  }
+    below600: `only screen and (max-width: 600px)`,
+    below786: `only screen and (max-width: 786px)`,
+    below992: `only screen and (max-width: 992px)`,
+    below1200: `only screen and (max-width: 1200px)`,
+  },
 };
